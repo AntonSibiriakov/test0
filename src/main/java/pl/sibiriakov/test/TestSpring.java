b713b6ec544144068f87c6209bfbaf3cbcff55ca
Anton Sibiriakov <tonan041@gmail.com>
2023-03-26 07:07:18 +0200

    2

--- src/main/java/pl/sibiriakov/test/TestSpring.java
+++ src/main/java/pl/sibiriakov/test/TestSpring.java
@@ -5,9 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
-
-        );
+                "applicationContext.xml");
 
         TestBean testBean = context.getBean("testBean",TestBean.class);
         System.out.println(testBean.getName());
